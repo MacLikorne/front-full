7b81bfb414b38b43ecbc8b6e926b35a415e6bc52
Enzo <e.radnai@hotmail.fr>
2020-10-30 19:07:26 +0100

    fix bugs

--- nginx/installer.sh
+++ nginx/installer.sh
@@ -1,8 +1,7 @@
 apt-get update && apt-get install apache2-utils -y
 
 # on ajoute un certificat ssl
-openssl req -x509 -nodes -days 365 -subj '/C=FR/ST=Chnord/L=Lille/CN=www.monsupersite.com' \
-    -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+openssl req -x509 -nodes -days 365 -subj '/C=FR/ST=Chnord/L=Lille/CN=www.monsupersite.com' -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 
 # on crée un groupe Diffie-Hellman pour augmenter le niveau de sécurité
 openssl dhparam -out /etc/nginx/dhparam.pem 1024 # 4096 recommandé car plus sécurisé
