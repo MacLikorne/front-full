8603834177b5b6f11dc1ef43565bc2216f7219c9
Enzo <e.radnai@hotmail.fr>
2020-11-02 14:46:23 +0100

    edit 404

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,7 +1,20 @@
 import './not-found.css'
+import body from '../../assets/nedry/body.png'
+import head from '../../assets/nedry/head.png'
+import hand from '../../assets/nedry/hand.png'
+import haha from '../../assets/nedry/ahahah.mp3'
 
 export const NotFound = props => {
     return (
-        <h1>Non trouvé</h1>
+        <div className="container" >
+            <div className="nedry">
+                <img className="nedry-body" src={body} />
+                <img className="nedry-head" src={head} />
+                <img className="nedry-hand" src={hand} />
+            </div>
+            <audio id="player" autoPlay loop>
+                <source src={haha} type="audio/mp3" />
+            </audio>
+        </div>
     )
 }
