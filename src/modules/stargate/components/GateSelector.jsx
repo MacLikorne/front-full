6aee49739c207d94a0b176303e15226f63b03556
Enzo <e.radnai@hotmail.fr>
2020-11-05 17:18:46 +0100

    ending stargate

--- src/modules/stargate/components/GateSelector.jsx
+++ src/modules/stargate/components/GateSelector.jsx
@@ -1,14 +1,12 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core'
-import { Fragment } from 'react'
 
 export const GateSelector = props => {
-    let gates = ['A', 'B', 'C', 'D']
 
     return (
         <FormControl >
             <label >Choisissez la porte à laquelle vous vous trouvez :</label>
             <Select value={props.gate} onChange={(e) => props.select(e.target.value)}>
-                {gates.map(gate => <MenuItem value={gate} key={gate}>{gate}</MenuItem>)}
+                {props.gates.map(gate => <MenuItem value={gate} key={gate}>{gate}</MenuItem>)}
             </Select>
         </FormControl>
     )
--- src/modules/stargate/layouts/gate/Gate.jsx
+++ src/modules/stargate/layouts/gate/Gate.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import { Button } from '@material-ui/core'
+import { Fragment, useState } from 'react'
 import { GateSelector } from '../../components/GateSelector'
+import { TicketInput } from '../../components/TicketInput'
+import StargateService from '../../services/stargate.service'
+import { StargateDto } from '../../models/stargate.dto'
+import './gate.css'
 
 export const Gate = props => {
-    const [gate, setGate] = useState("")
+    const [gate, setGate] = useState('A')
+    const [ticket, setTicket] = useState('A')
+    let gates = ['A', 'B', 'C', 'D']
+    const [way, setWay] = useState("")
+
+    const wayToGo = () => {
+        StargateService.getDirection(new StargateDto({ gate, ticket })).then(resp => setWay(resp))
+    }
 
     return (
-        <GateSelector gate={gate} select={setGate} />
+        <Fragment>
+            <div id='gate' className='input'>
+                <GateSelector gates={gates} gate={gate} select={setGate} />
+            </div>
+            <div id='ticket' className='input'>
+                <TicketInput gates={gates} ticket={ticket} select={setTicket} />
+            </div>
+            <Button id='way-button' onClick={wayToGo}>Demandez votre chemin</Button>
+            <div>
+                <label>{way}</label>
+            </div>
+        </Fragment>
     )
 }
